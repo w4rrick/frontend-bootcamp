752135a441301f48d49278be0e9e505089381736
Flynn <y4motcocserver@gmail.com>
2026-02-10 07:01:27 +0800

    Initial commit: basic HTML, CSS, JS setup

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("testBtn");
+
+  btn.addEventListener("click", () => {
+    console.log("Button clicked");
+  });
+});
